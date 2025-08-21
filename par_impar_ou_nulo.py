43e886ab3718b79e91a9e91396d5bf8abf1d8eed
Matheus Barcelos <matheusxboxbm@gmail.com>
2025-08-21 00:28:36 -0300

    Delete par_impar_ou_nulo.py

--- par_impar_ou_nulo.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Digite um número: "))
-
-if num > 0:
-    print(f"O número {num} é Positivo!")
-elif num == 0:
-    print(f"O número {num} é Nulo!")
-else:
-    print(f"O número {num} é Negativo!")
