97457dafe47fbf477a0f3a6fb88582e1aaf933f6
matrukis <matheusxboxbm@gmail.com>
2025-08-20 23:36:11 -0300

    Programa que diz se o input do usuário é par,negativo ou nulo

---
+++ par_impar_ou_nulo.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número: "))
+
+if num > 0:
+    print(f"O número {num} é Positivo!")
+elif num == 0:
+    print(f"O número {num} é Nulo!")
+else:
+    print(f"O número {num} é Negativo!")
